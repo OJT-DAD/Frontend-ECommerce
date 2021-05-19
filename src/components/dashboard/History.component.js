6839e0e40fba66629a3c716e369e583ff3c39207
Ardi-Hanafi <Ardi-Hanafi@users.noreply.github.com>
2021-05-19 23:17:56 +0700

    Admin History
    Admin History view

--- src/components/dashboard/History.component.js
+++ src/components/dashboard/History.component.js
@@ -15,8 +15,42 @@ const History = () => {
         </button>
         <h4>History Management</h4>
       </header>
+      <div className="con-right">
+        <table className="history">
+          <thead>
+            <tr>
+              <th>Store</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{Data.storeName}</td>
+              <td>{Data.userData.fullName}</td>
+              <td>{Data.dateTransactionFinish}</td>
+              <td>{Data.totalTransactionPrice}</td>
+              <td>{Data.statusTransaction === 1 && 
+                (<button>Success</button>)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
 
 export default History;
+
+const Data = {
+  'storeName': 'Rafi Store',
+  'userData': {
+    'fullName': 'Ardi Hanafi'
+  },
+  'dateTransactionFinish': '09-11-2021',
+  'totalTransactionPrice': 'Rp. 100.000,00',
+  'statusTransaction': 1
+}
